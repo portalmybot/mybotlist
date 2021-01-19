55466fead991ba907de8b648b950b2dd70de8a85
cratermaik <cratermaik@gmail.com>
2021-01-19 13:24:51 -0500

    update theme Dark

--- src/layouts/HeroContent.js
+++ src/layouts/HeroContent.js
@@ -4,12 +4,13 @@ import Typography from '@material-ui/core/Typography';
 
 
 const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
   heroContent: {
     backgroundColor: "theme.palette.background",
     padding: theme.spacing(1, 10, 5),
+  },
+  root: {
+    width: '100%',
+    maxWidth: 500,
   },
  })
 )
@@ -17,10 +18,10 @@ export default function HeroContent() {
   const classes = useStyles();
    return (
     <div className={classes.heroContent}>
-      <Typography component="h1" variant="h3" align="center" color="textPrimary" gutterBottom>
+      <Typography component="h1" variant="h3" align="center" color="text" gutterBottom>
         MyBOT List
       </Typography>
-      <Typography variant="h5" align="center" color="textSecondary" paragraph>
+      <Typography variant="h5" align="center" color="text" paragraph>
         Una plataforma para el listado de Bots publicos de discord en español,
         puede usar nuestra lista para descubrir nuevos bots desarrollados por los miembros
         de la comunidad MyBOT Team.
--- src/theme.js
+++ src/theme.js
@@ -1,23 +1,13 @@
-import { red } from '@material-ui/core/colors';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 // A custom theme for this app
 const theme = createMuiTheme({
   palette: {
+    type: 'dark',
     primary: {
       main: '#6930c3',
-    },
-    secondary: {
-      main: '#64dfdf',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#313131',
-      paper: '#252525',
-    },
-  },
+    }
+  }
 });
 
 export default theme;
