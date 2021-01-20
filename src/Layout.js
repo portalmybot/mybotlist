da20e845f16cfd37a345a285a00d64b1ab10aa1f
cratermaik <cratermaik@gmail.com>
2021-01-19 20:30:29 -0500

    update test cards list

--- src/Layout.js
+++ src/Layout.js
@@ -55,7 +55,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [1, 2, 3, 4, 5, 6];
 
 export default function Album() {
   const classes = useStyles();
