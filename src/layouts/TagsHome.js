fcf6eecb4e1d608c7623b7a9b73316a1cc784193
cratermaik <cratermaik@gmail.com>
2021-01-20 20:00:31 -0500

    update tags background Color Random

--- src/layouts/TagsHome.js
+++ src/layouts/TagsHome.js
@@ -4,6 +4,26 @@ import Chip from '@material-ui/core/Chip';
 
 const tags = ['Anime', 'Economía', 'Juegos', 'Música', 'Social', 'Utilidad', 'Moderación'];
 
+function stringToColor(string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.substr(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -12,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
     '& > *': {
       margin: theme.spacing(0.5),
     },
-    marginTop: theme.spacing(3)
+    marginTop: theme.spacing(3),
   },
 }));
 
@@ -22,7 +42,7 @@ export default function Chips() {
   return (
     <div className={classes.root}>
       {tags.map((tag) => (
-      <Chip label={tag} component="a" color="secondary" href={'tag/'+tag} clickable/>
+      <Chip label={tag} component="a" color="default" style={{backgroundColor: stringToColor(tag), fontSize: 15}} href={'tag/'+tag} clickable/>
       ))}
     </div>
   );
