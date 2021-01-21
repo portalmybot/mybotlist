6f2f4a7357d7e4236e58191f942b07055776cbdd
cratermaik <cratermaik@gmail.com>
2021-01-20 20:28:10 -0500

    add Cards, List Test

--- src/Layout.js
+++ src/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import Button from '@material-ui/core/Button';
+/* import Button from '@material-ui/core/Button';
 
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import CardMedia from '@material-ui/core/CardMedia'; */
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 
@@ -15,6 +15,7 @@ import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 
 import NavBar from './layouts/navBar.js';
+import CardList from './layouts/CardList.js';
 import HeroContent from './layouts/HeroContent.js';
 
 function Copyright() {
@@ -71,29 +72,7 @@ export default function Album() {
           <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
+                <CardList />
               </Grid>
             ))}
           </Grid>
