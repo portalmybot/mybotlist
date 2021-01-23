d711975360378bc539dfe84d53ab13d71050c29e
cratermaik <cratermaik@gmail.com>
2021-01-22 23:25:42 -0500

    update test

--- src/layouts/CardList.js
+++ src/layouts/CardList.js
@@ -75,8 +75,7 @@ export default function RecipeReviewCard() {
 
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+          This impressive paella is a perfect party dish and a fun meal to cooke.
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
