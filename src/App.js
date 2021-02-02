a742373b5bf8c233fe41b509fcf52d131f728049
cratermaik <cratermaik@gmail.com>
2021-02-02 14:04:46 -0500

    update links

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 
 import theme from './assets/theme';
 import Home from './pages/Home';
-/* import Register from './pages/Register';
-import AppBot from './pages/AddBot'; */
+import Register from './pages/Register';
+import AppBot from './pages/AddBot';
 import Login from './Login';
 
 
@@ -25,12 +25,12 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          {/* <Route path="/register">
+          <Route path="/register">
             <Register />
           </Route>
           <Route path="/add">
             <AppBot />
-          </Route> */}
+          </Route>
           <Route exact path="/auth/discord" component={Login} />
           <Redirect to="/" />
         </Switch>
