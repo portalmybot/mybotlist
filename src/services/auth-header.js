5768da76bf2f740d658b1dcb539f58b237377958
cratermaik <cratermaik@gmail.com>
2021-02-05 21:55:36 -0500

    update test encoded

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -2,6 +2,11 @@ const authHeader = () => {
   
   const tokenString = localStorage.getItem('token');
   const userToken = JSON.parse(tokenString);
+  const decodedStringBtoA = userToken;
+
+  // Encode the String
+  const encodedStringBtoA = btoa(decodedStringBtoA);
+  console.log(encodedStringBtoA);
 
   if (tokenString) {
     return { 
