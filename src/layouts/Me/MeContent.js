e38c574647fddbb8974365700fb53e1831e4c7d7
cratermaik <cratermaik@gmail.com>
2021-02-11 14:15:10 -0500

    update test profile

--- src/layouts/Me/MeContent.js
+++ src/layouts/Me/MeContent.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-
-
+import Avatar from '@material-ui/core/Avatar';
+import { red } from '@material-ui/core/colors';
 const useStyles = makeStyles((theme) => ({
   meContent: {
     padding: theme.spacing(5, 5, 20, 5),
     width: '100%',
     backgroundColor: theme.palette.background.paper,
+    
   },
   textContent: {
     maxWidth: 900,
     margin: theme.spacing(0, 'auto'),
-  }
+  },
+  avatar: {
+    backgroundColor: red[500],
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+  },
  })
 )
 
@@ -25,6 +31,9 @@ const MeContent = ({data}) => {
     
     <div className={classes.meContent}>
       <Typography component="h1" variant="h3" align="center" color="inherit"  gutterBottom>
+        <Avatar alt="Image title" align="center" /* src="https://source.unsplash.com/random" */ className={classes.avatar}>
+              BOT
+        </Avatar>
         <Box fontWeight="fontWeightBold" m={1}>
           Perfil
         </Box>
