438897ed6aa56432bb85a3f5f8cc1587566b9190
cratermaik <cratermaik@gmail.com>
2021-02-18 19:21:36 -0500

    add user_id for api add bot

--- src/services/bot.service.js
+++ src/services/bot.service.js
@@ -1,15 +1,18 @@
 import http from "./HttpService";
+import MeService from './me.service';
 
 export const getHomeBots = async () => {
   const response = await http.get("/home/bots");
   return response;
 
 };
+
 export const addBot = async ({ data }) => {
-  
+  const userLogin = await MeService()
+
   const postData = {
     id_bot: data.id_bot,
-    idUser_bot: data.idUser_bot ? data.idUser_bot : '12356',
+    idUser_bot: userLogin.user.user_id,
     prefix_bot: data.prefix_bot,
     shortDesc_bot: data.shortDesc_bot,
     invite_bot: data.invite_bot ? data.invite_bot : null,
@@ -17,6 +20,7 @@ export const addBot = async ({ data }) => {
 
   }
   console.log(postData);
+  console.log('user_id', userLogin.user.user_id);
   await http.post("/bots", postData);
 
 };
