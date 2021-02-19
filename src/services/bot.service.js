50f07b5beeb39a9032d6172553f7ddd4ff65b121
cratermaik <cratermaik@gmail.com>
2021-02-18 19:14:23 -0500

    add Data Post for Api register Bot

--- src/services/bot.service.js
+++ src/services/bot.service.js
@@ -6,9 +6,19 @@ export const getHomeBots = async () => {
 
 };
 export const addBot = async ({ data }) => {
-  console.log(data);
- /* await http.post("/bots", data);
- */
+  
+  const postData = {
+    id_bot: data.id_bot,
+    idUser_bot: data.idUser_bot ? data.idUser_bot : '12356',
+    prefix_bot: data.prefix_bot,
+    shortDesc_bot: data.shortDesc_bot,
+    invite_bot: data.invite_bot ? data.invite_bot : null,
+    note_bot: data.note_bot ? data.note_bot : null,
+
+  }
+  console.log(postData);
+  await http.post("/bots", postData);
+
 };
 export const addTags = async (data) => {
   console.log(data);
