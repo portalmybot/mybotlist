1bc72136006a74e50b247ffad86cf80af5310d24
cratermaik <cratermaik@gmail.com>
2021-02-22 19:51:51 -0500

    updated container page Bot

--- src/pages/Bot.js
+++ src/pages/Bot.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Layout from '../components/Layout';
-/* import ContainerBots from '../layouts/Home/ContainerBots.js';
- */
-import Typography from '@material-ui/core/Typography';
+
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -11,6 +9,9 @@ import Grid from '@material-ui/core/Grid';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  containerbg: {
+    margin: theme.spacing(1.5, 0)
   },
   paper: {
     padding: theme.spacing(2),
@@ -23,25 +24,30 @@ export default function Home() {
   const classes = useStyles();
   return (
     <Layout>
-      <Container maxWidth="lg">
-        <div className={classes.root}>
-          <Grid container spacing={3}>
+      <Container maxWidth={false} className={classes.containerbg} style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
+        <Container maxWidth={'lg'}>
+          <div className={classes.root}>
+            <Grid container spacing={3}>
 
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>xs=12</Paper>
+              <Grid item xs={6}>
+                <Paper className={classes.paper}>xs=6</Paper>
+              </Grid>
+              <Grid item xs={6}>
+                <Paper className={classes.paper}>xs=6</Paper>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>xs=12</Paper>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>xs=12</Paper>
+              </Grid>
             </Grid>
-            <Grid item xs={6}>
-              <Paper className={classes.paper}>xs=6</Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper className={classes.paper}>xs=6</Paper>
-            </Grid>
-          
-          </Grid>
-        </div>
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '50vh' }} />
+          </div>
+        
+        </Container>
       </Container>
-    {/*   <ContainerBots /> */}
     </Layout>
   );
 }
