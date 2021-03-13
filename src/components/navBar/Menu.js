96c0825f08a3fe4b5a34f3a5cfa8ebd84d42d7c8
cratermaik <cratermaik@gmail.com>
2021-03-12 22:03:58 -0500

    update Avatar Menu Auth

--- src/components/navBar/Menu.js
+++ src/components/navBar/Menu.js
@@ -2,15 +2,27 @@ import React from 'react';
 import Link from '@material-ui/core/Link';
 import { Link as RouterLink } from 'react-router-dom';
 
+import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import Avatar from '@material-ui/core/Avatar';
 
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
+
+const useStyles = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(4.5),
+    height: theme.spacing(4.5),
+  }
+}));
+
 export default function MenuAuth() {
+  const classes = useStyles();
+
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -28,8 +40,9 @@ export default function MenuAuth() {
           onClick={handleMenu}
           color="inherit"
         >
-        <AccountCircle />
+        <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/2.jpg" className={classes.small} />
       </IconButton>
+
       <Menu
           id="menu-appbar"
           anchorEl={anchorEl}
