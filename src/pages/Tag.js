5f42adab3e366477f429fbdb428eb0ae5861ee53
cratermaik <cratermaik@gmail.com>
2021-04-25 21:28:27 -0500

    update page tag

--- src/pages/Tag.js
+++ src/pages/Tag.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import HeroContent from '../layouts/Home/HeroContent.js';
-import ContainerBots from '../layouts/Home/ContainerBots.js';
 
 export default function Home() {
   return (
     <Layout >
       <HeroContent />
-      {/* <ContainerBots /> */}
     </Layout>
   );
 }
--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -4,7 +4,7 @@ import CookieService from "./CookieService";
 const at = CookieService.get("access_token");
 
 export default axios.create({
-  baseURL: "http://127.0.0.1:3232/api/v1",
+  baseURL: "http://127.0.0.1:8000/api/v1",
   headers: {
     "Accept": "application/json",
     "Authorization": "Bearer " + at,
