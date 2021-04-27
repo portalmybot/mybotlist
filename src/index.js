602489a80af59c9ea14b7a041a788a3cf02b68dd
cratermaik <cratermaik@gmail.com>
2021-04-26 22:26:57 -0500

    update index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import reportWebVitals from './reportWebVitals';
-/* import axiosHeaders from './axiosHeaders'; */
+
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { SnackbarProvider } from 'notistack';
