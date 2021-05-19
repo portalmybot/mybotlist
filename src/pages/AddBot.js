b5895b7cfe173352a686df678f42e4645366bd78
cratermaik <cratermaik@gmail.com>
2021-05-18 22:46:05 -0500

    update addBot

--- src/pages/AddBot.js
+++ src/pages/AddBot.js
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(5)
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: '100%',
     marginTop: theme.spacing(3),
   },
   submit: {
