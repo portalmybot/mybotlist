d89ed28329ea95f1f766c9a32e3c51ebea6beffd
cratermaik <cratermaik@gmail.com>
2021-05-19 22:41:41 -0500

    update tagHome

--- src/layouts/Home/TagsHome.js
+++ src/layouts/Home/TagsHome.js
@@ -11,7 +11,6 @@ function stringToColor(string) {
   let hash = 0;
   let i;
 
-  /* eslint-disable no-bitwise */
   for (i = 0; i < string.length; i += 1) {
     hash = string.charCodeAt(i) + ((hash << 5) - hash);
   }
