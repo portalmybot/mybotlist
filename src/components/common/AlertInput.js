1e4b3e569a8405175a495aa2d11afd2f0fd7c6ac
cratermaik <cratermaik@gmail.com>
2021-05-24 23:00:07 -0500

    Update Alertinput

--- src/components/common/AlertInput.js
+++ src/components/common/AlertInput.js
@@ -16,18 +16,6 @@ const DescriptionAlerts = () => {
 
   return (
     <div className={classes.root}>
-      {/* <Alert severity="error">
-        <AlertTitle>Error</AlertTitle>
-        This is an error alert — <strong>check it out!</strong>
-      </Alert>
-      <Alert severity="warning">
-        <AlertTitle>Warning</AlertTitle>
-        This is a warning alert — <strong>check it out!</strong>
-      </Alert>
-      <Alert severity="info">
-        <AlertTitle>Info</AlertTitle>
-        This is an info alert — <strong>check it out!</strong>
-      </Alert> */}
       <Alert severity="success">
         <AlertTitle>Success</AlertTitle>
         Su BOT fue agreado correctamente.
