85fdbc80a14406f08337ef704cd416d2047808b4
cratermaik <cratermaik@gmail.com>
2021-05-29 21:22:08 -0500

    update theme

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -1,7 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 const font = "'Lexend Deca', sans-serif";
 
-// A custom theme for this app
 const theme = createMuiTheme({
   typography: {
     fontFamily: font
