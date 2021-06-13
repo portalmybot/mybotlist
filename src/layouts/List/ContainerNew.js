cc10c0ca959bbae05278728ad5affd3efa5cae29
cratermaik <cratermaik@gmail.com>
2021-06-13 11:55:48 -0500

    Update Containers Bots

--- src/layouts/List/ContainerNew.js
+++ src/layouts/List/ContainerNew.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { cyan } from '@material-ui/core/colors';
 import { useQuery } from 'react-query'
+import FiberNewIcon from '@material-ui/icons/FiberNew';
 
 import Bot from '../Home/Bot';
 import BotLoading from '../../components/skeleton/BotLoading';
@@ -14,6 +17,20 @@ const useStyles = makeStyles((theme) => ({
   cardGrid: {
     paddingBottom: theme.spacing(8),
     marginTop: theme.spacing(-10),
+  },
+  textContent: {
+    display: 'flex',
+    
+  },
+  textSubtitle: {
+    marginLeft: theme.spacing(6),
+    marginTop: theme.spacing(-4.5),
+    marginBottom: theme.spacing(2)
+  },
+  iconText: {
+    marginRight: theme.spacing(1),
+    fontSize: theme.spacing(5),
+    marginTop: theme.spacing(.3)
   }
  })
 )
@@ -27,6 +44,14 @@ const ListNewsBots = () => {
   return (
     <>
       <Container className={classes.cardGrid} maxWidth="lg">
+        <Box m="auto">
+          <Typography variant="subtitle1" component="h1" color="inherit" className={classes.textContent} paragraph>
+            <FiberNewIcon className={classes.iconText} style={{ color: cyan['700'] }} /> DISCORD BOTS RECIENTES
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom className={classes.textSubtitle}>
+            ¡Lista de bots recientemente agregados a la lista!
+          </Typography>
+        </Box>
         <Grid container spacing={5}>
           {isLoading && (
             <>
--- src/layouts/List/ContainerVerified.js
+++ src/layouts/List/ContainerVerified.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { green } from '@material-ui/core/colors';
+import Box from '@material-ui/core/Box';
 
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import { useQuery } from 'react-query'
 
 import Bot from '../Home/Bot';
@@ -14,6 +18,20 @@ const useStyles = makeStyles((theme) => ({
   cardGrid: {
     paddingBottom: theme.spacing(8),
     marginTop: theme.spacing(-10),
+  },
+  textContent: {
+    display: 'flex',
+    
+  },
+  textSubtitle: {
+    marginLeft: theme.spacing(6),
+    marginTop: theme.spacing(-4.5),
+    marginBottom: theme.spacing(2)
+  },
+  iconText: {
+    marginRight: theme.spacing(1),
+    fontSize: theme.spacing(5),
+    marginTop: theme.spacing(.3)
   }
  })
 )
@@ -27,6 +45,14 @@ const ListVerifiedBots = () => {
   return (
     <>
       <Container className={classes.cardGrid} maxWidth="lg">
+        <Box m="auto">
+          <Typography variant="subtitle1" component="h1" color="inherit" className={classes.textContent} paragraph>
+            <VerifiedUserIcon className={classes.iconText} style={{ color: green['A400'] }} /> DISCORD BOTS VERIFICADOS
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom className={classes.textSubtitle}>
+            ¡Son bots de confianza certificados por MyBOT Team!
+          </Typography>
+        </Box>
         <Grid container spacing={5}>
           {isLoading && (
             <>
