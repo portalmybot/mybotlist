5dd20b61470c31786d1da841bd2d994088e17e6e
cratermaik <cratermaik@gmail.com>
2021-06-20 23:42:14 -0500

    Update ActionBot add Link Router

--- src/components/bot/ActionBot.js
+++ src/components/bot/ActionBot.js
@@ -4,6 +4,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
+import Link from '@material-ui/core/Link';
+import { Link as RouterLink } from 'react-router-dom';
+
 const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(0, .5, 1.6),
@@ -12,13 +15,15 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ActionBot({ bot }) {
   const classes = useStyles();
-  const { vote_bot, serverCount_bot } = bot;
+  const { id_bot, vote_bot, serverCount_bot } = bot;
   return (
     <div>
-      <ButtonGroup aria-label="small outlined button group">
-        <Button variant="contained" style={{ color: '#fff', backgroundColor: '#e91e63' }}>{vote_bot} Rep.</Button>
-        <Button variant="contained" style={{ color: '#fff', backgroundColor: '#c50546' }}> <FavoriteIcon /> </Button>
-      </ButtonGroup>
+        <Link underline='none' component={RouterLink} to={'/bot/'+id_bot+'/vote'} color="inherit">
+          <ButtonGroup aria-label="small outlined button group">
+            <Button variant="contained" style={{ color: '#fff', backgroundColor: '#e91e63' }}>{vote_bot} Rep.</Button>
+            <Button variant="contained" style={{ color: '#fff', backgroundColor: '#c50546' }}> <FavoriteIcon /> </Button>
+          </ButtonGroup>
+        </Link>
       {serverCount_bot && (
         <ButtonGroup color="primary" aria-label="outlined primary button group">
           <Button
