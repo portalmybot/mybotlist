163a0355f759f2a86f76636313af7169c21d192a
cratermaik <cratermaik@gmail.com>
2021-07-17 22:30:32 -0500

    Update botservice2

--- src/services/bot.service.js
+++ src/services/bot.service.js
@@ -189,11 +189,12 @@ export const addVote = async (data) => {
 export const addTags = async (data) => {
   const id_bot = data.id_bot;
   data.tags.map(async (tag) => {
+    
     const postData = {
       id_bot: id_bot,
       name_tag: tag,
     }
-    
+
     await http.post("/bots/add/tag", postData);
   })
 
