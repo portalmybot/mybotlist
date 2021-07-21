f592b2ba4c21cf618e82f986d1c414fd8a9039c6
cratermaik <cratermaik@gmail.com>
2021-07-21 14:56:43 -0500

    Update PayPaypal add PayPalButton

--- src/components/common/PayPaypalPerm.js
+++ src/components/common/PayPaypalPerm.js
@@ -1,5 +1,5 @@
-import React, { useState, useRef, useEffect } from 'react';
-
+import React, { useState, useRef } from 'react';
+import { PayPalButton } from 'react-paypal-button-v2'
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import { addUserPremiumPerm } from '../../services/me.service';
 import Avatar from '@material-ui/core/Avatar';
@@ -9,48 +9,15 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import { green, grey } from '@material-ui/core/colors';
 import { useMutation } from "react-query";
 import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
+
 import { Typography } from '@material-ui/core';
 
 export default function PayPayPalPerm() {
   const [paid, setPaid] = useState(false);
   const [error, setError] = useState(null);
-  const paypalRef = useRef();
+
   const timer = useRef();
   const mutate = useMutation(addUserPremiumPerm);
-
-  useEffect(() => {
-    clearTimeout(timer.current);
-    window.paypal
-      .Buttons({
-        createOrder: (data, actions) => {
-          return actions.order.create({
-            intent: "CAPTURE",
-            purchase_units: [
-              {
-                description: "MyBOT List Premium Permanente",
-                amount: {
-                  currency_code: "USD",
-                  value: 12.0,
-                },
-              },
-            ],
-          });
-        },
-        onApprove: async (data, actions) => {
-          /* const order = await actions.order.capture(); */
-          setPaid(true);
-          mutate.mutate();
-          
-        },
-        onError: (err) => {
-          setError(err)
-          console.error(err);
-        },
-      })
-      .render(paypalRef.current);
-      
-  }, []);
 
   if (paid) {
      timer.current = window.setTimeout(() => {
@@ -85,26 +52,42 @@ export default function PayPayPalPerm() {
         <ListItemText primary="Total: $12 Dolares" secondary="MyBOT List Premium Permanente" />
       </ListItem>
       
-      <div ref={paypalRef} />
-      <Grid item xs={12} sm={12}>
-          <Box display="flex">
-            <Box m="auto">
-              <Typography variant="subtitle1" gutterBottom>
-                Condiciones
-              </Typography>
-              <Typography variant="subtitle2" gutterBottom>
-                Las compras son únicas para recibir los beneficios. 
-                Puedes comprar tantas veces como quieras y se acumularán.
-                Una compra solo se puede transferir a un usuario diferente una vez. 
-                No se permite la reventa de Premium.
-              </Typography>
-              <Typography variant="subtitle2" gutterBottom>
-                Si tiene alguna pregunta adicional o desea solicitar un reembolso, no dude en contactarnos.
-              </Typography>
+      <PayPalButton
+        amount = '12.00'
+        onSuccess = {(details, data) => {
+          console.log(details.payer)
+          console.log(data.orderID)
+          setPaid(true);
+          mutate.mutate();
+        }}
+        catchError = {(err) => {
+          setError(err)
+          console.error(err);
+        }}
+        options = {{
+          clientId: `${process.env.REACT_APP_CLIENT_PAYPAL}`
+        }}
+      >
+      </PayPalButton>
+
+      <Box display="flex">
+        <Box m="auto">
+          <Typography variant="subtitle1" gutterBottom>
+             Condiciones
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom>
+            Las compras son únicas para recibir los beneficios. 
+            Puedes comprar tantas veces como quieras y se acumularán.
+            Una compra solo se puede transferir a un usuario diferente una vez. 
+            No se permite la reventa de Premium.
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom>
+            Si tiene alguna pregunta adicional o desea solicitar un reembolso, no dude en contactarnos.
+          </Typography>
               
-            </Box>
-          </Box>
-        </Grid>
+        </Box>
+      </Box>
+
     </>
   );
 }
